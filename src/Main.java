4412111e3b357f58082780129fce05e15af2bd72
jameshlms <j.r.holmes@outlook.com>
2024-02-12 12:28:05 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Import your package
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Create two lists using the two different constructors.
+         */
+
+        /*
+        Print the lists as arrays using one of the methods you have made.
+         */
+
+        /*
+        Add 3 elements to your empty list and to your populated list.
+         */
+
+        /*
+        Now utilize all the methods you've made however you'd like.
+         */
+
+        /*
+        Print out both lists using the toString() method.
+         */
+    }
+}
---
+++ src/tutoriallist/TutorialList.java
@@ -0,0 +1,20 @@
+package tutoriallist;
+
+public class TutorialList {
+    /*
+    Declare the instance variables of the class.
+    We want to have just an integer array called "data"
+    and an integer called "size".
+     */
+
+    /*
+    Create two constructor methods.
+    One should take no arguments and initializes data as an empty array and size to 0.
+    The second should take one parameter being an array and data should be initialized to the argument array
+    and size should be the length of the argument array.
+     */
+
+    /*
+    Create the methods 'size()', 'get()', 'set()', two 'add()' methods, 'swap()', 'toArray()' and 'toString()'
+     */
+}
